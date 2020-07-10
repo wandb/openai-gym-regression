0abd794d84b781850017ca686b9373a45951b20a
David Jackson <1735971+davidwallacejackson@users.noreply.github.com>
2020-07-10 14:46:49 -0700

    Add files via upload

---
+++ train.py
@@ -0,0 +1,10 @@
+import gym
+import wandb
+
+wandb.init(project='openai-gym-regression', monitor_gym=True)
+env = gym.make('CartPole-v0')
+env.reset()
+for _ in range(1000):
+    env.render()
+    env.step(env.action_space.sample()) # take a random action
+env.close()
